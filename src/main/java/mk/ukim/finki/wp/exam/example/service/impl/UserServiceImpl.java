f877f9f6726f25947ff6c5b19e3a30b5c516607b
Ljubomir <ljupcedavitkov14@gmail.com>
2024-01-22 02:48:25 +0100

    Implements methods 3 - Encrypt password

--- src/main/java/mk/ukim/finki/wp/exam/example/service/impl/UserServiceImpl.java
+++ src/main/java/mk/ukim/finki/wp/exam/example/service/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package mk.ukim.finki.wp.exam.example.service.impl;
 
 import mk.ukim.finki.wp.exam.example.model.Role;
 import mk.ukim.finki.wp.exam.example.model.User;
+import mk.ukim.finki.wp.exam.example.repository.UserRepository;
 import mk.ukim.finki.wp.exam.example.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,8 +17,18 @@ public class UserServiceImpl implements UserService {
      * @param role
      * @return
      */
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public User create(String username, String password, Role role) {
-        return null;
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User(username, encodedPassword, role);
+        return userRepository.save(user);
     }
 }
